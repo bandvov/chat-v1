bb9b90d17fe98ed5d9064c31d486eeff38e992ba
bandvov <48312647+bandvov@users.noreply.github.com>
2021-04-17 21:24:56 +0300

    added PORT

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -9,7 +9,7 @@ import { ROUTES, PATH } from "../configs/routes";
 const RegisterPage = ({ history }) => {
   const onFinish = async (userData) => {
     await axios
-      .post(PATH + ROUTES.register, userData)
+      .post(PATH + ROUTES.register + process.env.PORT, userData)
       .then((res) => {
         notification.success({
           message: res.data.message,
