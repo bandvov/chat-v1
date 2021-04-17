8eba769f186b1d46f8d14edf552233371710fdf0
bandvov <bandvov@gmail.com>
2021-04-17 17:50:31 +0300

    updated user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,6 @@
 const User = require("../models/User");
 const sha256 = require("sha256");
 const jwt = require("jsonwebtoken");
-const { validateEmail, validatePassword } = require("../midlware/validators");
 
 module.exports.register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -27,10 +26,33 @@ module.exports.login = async (req, res) => {
   if (!user) {
     throw "User not found";
   }
-  const token = jwt.sign({ email: user.email, id:user._id }, process.env.SALT, {
-    expiresIn: "24h",
+  const token = jwt.sign(
+    { email: user.email, id: user._id },
+    process.env.SALT,
+    {
+      expiresIn: "24h",
+    }
+  );
+  res.status(200).json({
+    name: user.name,
+    userId: user._id,
+    token,
+    message: "Successfully logged in!",
   });
-  res
-    .status(200)
-    .json({ name: user.name,userId: user._id, token, message: "Successfully logged in!" });
 };
+
+module.exports.getAllUsers = async (req, res) => {
+  const users = await User.aggregate([
+    { $group: { _id: null, users: { $push: "$$ROOT" }, count: { $sum: 1 } } },
+    {
+      $project: {
+        _id: 0,
+      },
+    },
+  ]);
+  if (users) {
+    res.status(200).json(users[0]);
+  }
+};
+
+module.exports.getUserById = async (req, res) => {};
