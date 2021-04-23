31f232d6cce832680307a74386766b02be782e02
bandvov <bandvov@gmail.com>
2021-04-23 09:30:39 +0300

    changed public dir

--- server/app.js
+++ server/app.js
@@ -11,7 +11,10 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname + "public")));
+app.use(express.static(path.join(__dirname + "../client/build")));
+app.any("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // setup routes
 app.use(require("./routes/chatroom"));
